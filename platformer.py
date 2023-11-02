e867bd227d1443b3b05d4641a39e8ae40a641cd3
Alexander Diaz <AlexDiaz6022000@gmail.com>
2023-11-02 14:33:14 -0700

    created mulitple game levels

--- platformer.py
+++ platformer.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame.locals import *
+import pickle
+from os import path
+
 
 pygame.init()
 
@@ -7,7 +10,7 @@ clock = pygame.time.Clock()
 fps = 60
 
 screen_width = 1000
-screen_height = 850
+screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
@@ -16,6 +19,8 @@ pygame.display.set_caption('Platformer')
 tile_size = 50
 game_over = 0
 main_menu = True
+level = 1
+max_levels = 7
 
 # loading images
 sun_img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/sun.png')
@@ -23,6 +28,19 @@ bg_img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/sky.png
 restart_img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/restart_btn.png')
 start_img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/start_btn.png')
 exit_img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/exit_btn.png')
+
+
+# function to reset level
+def reset_level(level):
+    player.reset(10, screen_height - 130)
+    blob_group.empty()
+    lava_group.empty()
+    exit_group.empty()
+    if path.exists(f'/Users/alexanderdiaz/Desktop/Platformer-Game/level{level}_data'):
+        pickle_in = open(f'/Users/alexanderdiaz/Desktop/Platformer-Game/level{level}_data', 'rb')
+        world_data = pickle.load(pickle_in)
+    world = World(world_data)
+    return world
 
 
 class Button():
@@ -59,13 +77,15 @@ class Player():
     def __init__(self, x, y):
         self.reset(x, y)
 
+
+
     def update(self, game_over):
-        # dx and dy is change in direction
         dx = 0
         dy = 0
-        walk_cooldown = 5  # 20 iterations need to pass before updating the index
+        walk_cooldown = 5
+
         if game_over == 0:
-            # get key presses
+            #get keypresses
             key = pygame.key.get_pressed()
             if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
                 self.vel_y = -15
@@ -83,12 +103,13 @@ class Player():
             if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
                 self.counter = 0
                 self.index = 0
-                if self.direction == 1:  # checking if stopping after moving to the right,show right. the same for the left
+                if self.direction == 1:
                     self.image = self.images_right[self.index]
                 if self.direction == -1:
                     self.image = self.images_left[self.index]
 
-            # handle animation
+
+            #handle animation
             if self.counter > walk_cooldown:
                 self.counter = 0
                 self.index += 1
@@ -98,67 +119,81 @@ class Player():
                     self.image = self.images_right[self.index]
                 if self.direction == -1:
                     self.image = self.images_left[self.index]
-            # add gravity
+
+
+            #add gravity
             self.vel_y += 1
             if self.vel_y > 10:
                 self.vel_y = 10
             dy += self.vel_y
-            # check for collision
+
+            #check for collision
             self.in_air = True
             for tile in world.tile_list:
-                # check for collision in x direction
+                #check for collision in x direction
                 if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                     dx = 0
-                # check for collision in y direction
+                #check for collision in y direction
                 if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                    # check if below the ground (collision from under the block when player hits block from below)
+                    #check if below the ground i.e. jumping
                     if self.vel_y < 0:
                         dy = tile[1].bottom - self.rect.top
                         self.vel_y = 0
-                        # checking if landing on the block, hit the block from above
+                    #check if above the ground i.e. falling
                     elif self.vel_y >= 0:
                         dy = tile[1].top - self.rect.bottom
                         self.vel_y = 0
-                        self.in_air = False  # False now that player has landed on something, now he can jump again
-            # check for collision with enemies
+                        self.in_air = False
+
+
+            #check for collision with enemies
             if pygame.sprite.spritecollide(self, blob_group, False):
                 game_over = -1
-            # check for collision with lava
+
+            #check for collision with lava
             if pygame.sprite.spritecollide(self, lava_group, False):
                 game_over = -1
-                print(game_over)
 
-            # update player coordinates
+            #check for collision with exit
+            if pygame.sprite.spritecollide(self, exit_group, False):
+                game_over = 1
+
+
+            #update player coordinates
             self.rect.x += dx
             self.rect.y += dy
+
+
         elif game_over == -1:
             self.image = self.dead_image
             if self.rect.y > 200:
                 self.rect.y -= 5
-            # draw player on the screen
+
+        #draw player onto screen
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
         return game_over
 
+
     def reset(self, x, y):
         self.images_right = []
         self.images_left = []
-        self.index = 0  # which item from the list i want
-        self.counter = 0  # control the speed the animation runs through
+        self.index = 0
+        self.counter = 0
         for num in range(1, 5):
             img_right = pygame.image.load(f'/Users/alexanderdiaz/Desktop/Platformer-Game/guy{num}.png')
             img_right = pygame.transform.scale(img_right, (40, 80))
-            img_left = pygame.transform.flip(img_right, True, False)  # second arg is to flip from left to right
-            self.images_right.append(img_right)  # third arg is to flip from up and down
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
             self.images_left.append(img_left)
         self.dead_image = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/ghost.png')
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.width = self.image.get_width()  # get width of image
-        self.height = self.image.get_height()  # get height of image
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.jumped = False
         self.direction = 0
@@ -195,6 +230,9 @@ class World():
                 if tile == 6:
                     lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
                     lava_group.add(lava)  # creating the lava
+                if tile == 8:
+                    exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    exit_group.add(exit)
                 col_count += 1
             row_count += 1
 
@@ -232,33 +270,29 @@ class Lava(pygame.sprite.Sprite):
         self.rect.y = y
 
 
+class Exit(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)  # calling from super class
+        img = pygame.image.load('/Users/alexanderdiaz/Desktop/Platformer-Game/exit.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
 # 0 is blank space, 1 is dirt, 2 is grass
 world_data = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
-    [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
-    [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-    [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
-    [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
 ]
 
-player = Player(100, screen_height - 130)
+player = Player(150, screen_height - 130)
 blob_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+# load in level data and create world
+if path.exists(f'/Users/alexanderdiaz/Desktop/Platformer-Game/level{level}_data'):
+    pickle_in = open(f'/Users/alexanderdiaz/Desktop/Platformer-Game/level{level}_data', 'rb')
+    world_data = pickle.load(pickle_in)
 world = World(world_data)
 # create buttons
 restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
@@ -285,13 +319,31 @@ while run:
 
         blob_group.draw(screen)
         lava_group.draw(screen)
+        exit_group.draw(screen)
         game_over = player.update(game_over)
 
         # if player has died
         if game_over == -1:
             if restart_button.draw():
-                player.reset(100, screen_height - 130)
+                world_data = []
+                world = reset_level(level)
                 game_over = 0
+        # if player has complete the level
+        if game_over == 1:
+            # reset the game and go to next level
+            level += 1
+            if level <= max_levels:
+                # reset level
+                world_data = []
+                world = reset_level(level)
+                game_over = 0
+            else:
+                # restart game
+                if restart_button.draw():
+                    level = 1
+                    world_data = []
+                    world = reset_level(level)
+                    game_over = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
